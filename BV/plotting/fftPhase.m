65df74a0d706ce1742286cbab921aa6116b693fc
dinossht <dinosshan1996@hotmail.com>
2019-03-18 08:01:53 +0100

    Update

--- BV/plotting/fftPhase.m
+++ BV/plotting/fftPhase.m
@@ -1,5 +1,6 @@
-function fftPhase(data,L,fs)
-Y = fft(data(1,:));
+function fftPhase(data,fs)
+Y = fft(data);
+L = length(data);
 P2 = angle(Y/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
--- BV/scripts/x4_puls.m
+++ BV/scripts/x4_puls.m
@@ -1,5 +1,5 @@
 x4_specs;
-%tc = gauspuls('cutoff',fc,bw,[],-40); 
-%t = -tc : 1e-6 : tc; 
+tc = gauspuls('cutoff',fc,bw,[],-40); 
+t = -tc : 1e-6 : tc; 
 x4puls = gauspuls(t,fc,bw/fc); 
 plot(t,x4puls)
